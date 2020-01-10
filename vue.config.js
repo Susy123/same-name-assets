00a88b36084a71b393e7a2be20526b7ff3347326
Susy123 <chenxiaohui67@163.com>
2020-01-10 15:24:13 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
-    ‘’‘javascript
+module.exports = {
+    publicPath: './',
+    outputDir: 'same-name-assets',
     // 由于业务需求不做hash，以免服务器滞留大量文件，至于缓存利用，由app内部的更新机制做处理；
-    // 但这会导致了同名资源的问题
+    // 但这导致了同名资源的问题
     filenameHashing: false,
     // 修改 images 的 loader，增加 4 位 hash的方式来避免同名覆盖问题；通过 vue inpect查看是否修改正确
     chainWebpack: config => {
@@ -12,4 +14,4 @@
             return options
         })
     }
-    ’‘’
+}
